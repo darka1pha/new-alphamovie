2eabf7cf5227a58509adea8073c5ab7768a87484
DarkAlpha <abolfazl.omrani1999@gmail.com>
2022-09-15 15:17:03 +0430

    fix errors

--- components/PinInput/utils.tsx
+++
@@ -1,8 +0,0 @@
-export const removeValuesFromArray = (valuesArray: string[], value: string) => {
-
-  const valueIndex = valuesArray.findIndex(entry => entry === value)
-  if(valueIndex === -1) {
-    return
-  }
-  valuesArray.splice(valueIndex, 1)
-}
