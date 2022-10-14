c8353251b41ad4e506b431a6301a495227a62d79
DarkAlpha <abolfazl.omrani1999@gmail.com>
2022-10-14 22:47:19 +0330

    details interfaces

--- API/interfaces/index.ts
+++ API/interfaces/index.ts
@@ -1,8 +1,8 @@
 export interface ListResults {
 	adult: boolean
-	backdrop_path: string
+	backdrop_path: string | null
 	first_air_date: string
-	media_type: 'tv' | 'movie'
+	media_type: 'tv' | 'movie' | 'person'
 	name: string
 	overview: string
 	poster_path: string
@@ -19,6 +19,46 @@ export interface IPaginatedData<T> {
 }
 
 export interface TrendingsParams {
-	media_type: 'all' | 'movie' | 'tv'
+	media_type: 'all' | 'movie' | 'tv' | 'person'
 	pageParam?: number
+}
+
+export interface MovieDetails {
+	id: number
+	adult: boolean
+	backdrop_path: string | null
+	budget: number
+	genres: Array<{
+		name: string
+	}>
+	original_title: string
+	poster_path: string | null
+	vote_average: number
+	video: boolean
+	title: string
+	release_date: string
+	runtime: number
+	tagline: string | null
+	overview: string
+	status: string
+}
+
+export interface TvDetails {
+	id: number
+	backdrop_path: string | null
+	episode_run_time: Array<number>
+	first_air_date: string
+	last_air_date: string
+	number_of_episodes: number
+	number_of_seasons: number
+	genres: Array<{
+		name: string
+	}>
+	original_name: string
+	poster_path: string | null
+	vote_average: number
+	video: boolean
+	tagline: string | null
+	overview: string
+	status: string
 }
