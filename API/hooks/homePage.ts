75f013f127477bf186b9f2b0ce7dbf0d787747b0
DarkAlpha <abolfazl.omrani1999@gmail.com>
2022-10-14 22:46:43 +0330

    search added

--- API/hooks/homePage.ts
+++ API/hooks/homePage.ts
@@ -1,6 +1,6 @@
 import Axios from 'API/axios'
 import { IPaginatedData, ListResults, TrendingsParams } from 'API/interfaces'
-import { TRENDINGS } from 'API/urls'
+import { MULTI_SEARCH, TRENDINGS } from 'API/urls'
 import { useInfiniteQuery } from '@tanstack/react-query'
 
 export const useGetTrendings = ({ media_type }: TrendingsParams) =>
@@ -11,6 +11,21 @@ export const useGetTrendings = ({ media_type }: TrendingsParams) =>
 			return data
 		},
 		{
+			getNextPageParam: (lastPage, pages) => {
+				return lastPage.page + 1
+			},
+		}
+	)
+
+export const useGetMultiSearch = ({ query }: { query: string }) =>
+	useInfiniteQuery(
+		['search', query],
+		async ({ pageParam = 1 }): Promise<IPaginatedData<ListResults>> => {
+			const { data } = await Axios.get(MULTI_SEARCH(query, pageParam))
+			return data
+		},
+		{
+			enabled: !!query,
 			getNextPageParam: (lastPage, pages) => {
 				return lastPage.page + 1
 			},
