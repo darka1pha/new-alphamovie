5b70b3e64a75db89457537617d37b6948f4385a1
DarkAlpha <abolfazl.omrani1999@gmail.com>
2022-10-16 07:11:08 +0330

    fix skeleton styles

--- components/Card/Skeleton.tsx
+++ components/Card/Skeleton.tsx
@@ -2,7 +2,7 @@ const Skeleton = () => {
 	return (
 		<div
 			className={`flex flex-col bg-primary-300/10 animate-pulse
-			w-[250px] h-[400px] p-[8px] backdrop-blur-sm rounded-md cursor-pointer group m-4 flex-shrink flex-grow basis-[20%]`}></div>
+			w-[250px] h-[400px] p-[8px] backdrop-blur-sm rounded-md m-4`}></div>
 	)
 }
 
