2f5fac285229dc606f194ada57ab355dea8cbaa5
DarkAlpha <abolfazl.omrani1999@gmail.com>
2022-10-16 07:12:34 +0330

    fix styles

--- layouts/ThemeProvider.tsx
+++ layouts/ThemeProvider.tsx
@@ -11,7 +11,7 @@ const ThemeProvider: NextPage<Props> = (props) => {
 		<div className="overflow-x-hidden flex justify-center w-full bg-cover bg-no-repeat bg-primary-900 bg-[url('/assets/bg.webp')]">
 			<div className='max-w-[1920px] overflow-y-hidden w-full min-h-screen'>
 				<Navbar />
-				<div className='h-full w-full px-4 sm:px-12  md:px-18 lg:px-24 overflow-x-hidden pt-10'>
+				<div className='h-full w-full px-4 sm:px-12  md:px-18 lg:px-24 overflow-x-hidden pt-[90px]'>
 					{props.children}
 				</div>
 			</div>
